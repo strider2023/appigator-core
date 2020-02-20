ba38883e7133d3d92924af4ac70348e132c5042f
Arindam Nath <arindam.nath@optimizeitsystems.com>
2020-02-20 15:35:48 +0530

    Added date time picker

---
+++ projects/appigator-core/src/lib/core-field-web/date-time-picker/date-time-picker.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-date-time-picker-w',
+  templateUrl: './date-time-picker.component.html',
+  styleUrls: ['./date-time-picker.component.scss'],
+})
+export class DateTimePickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
